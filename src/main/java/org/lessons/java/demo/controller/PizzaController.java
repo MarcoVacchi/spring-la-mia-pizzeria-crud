cbd05b51ee776dc0970d48b83a95facb2772b4bf
MarcoVacchi <mvacchi816@gmail.com>
2025-07-24 16:04:21 +0200

    feat: created show

--- src/main/java/org/lessons/java/demo/controller/PizzaController.java
+++ src/main/java/org/lessons/java/demo/controller/PizzaController.java
@@ -1,6 +1,5 @@
 package org.lessons.java.demo.controller;
 
-import java.util.ArrayList;
 import java.util.List;
 
 import org.lessons.java.demo.model.Pizzeria;
@@ -10,6 +9,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 @Controller
 @RequestMapping("/pizze")
@@ -19,6 +19,7 @@ public class PizzaController {
     @Autowired
     private PizzaRepository repository;
 
+    // index
     @GetMapping
     public String index(Model model) {
 
@@ -27,4 +28,22 @@ public class PizzaController {
         return "pizze/index";
     }
 
+    // show
+    @GetMapping("/{id}")
+    public String show(@PathVariable("id") Integer id, Model model) {
+
+        Pizzeria pizza = repository.findById(id).get();
+
+        model.addAttribute("pizza", pizza);
+        return "pizze/show";
+    }
+
 }
+// Mostriamo una singola pizza.
+
+// Ogni pizza dell’elenco avrà quindi un pulsante che se cliccato ci porterà a
+// una pagina che mostrerà i dettagli della pizza scelta. Dobbiamo quindi
+// inviare l’id come parametro dell’URL, recuperarlo nel metodo del controller,
+// caricare i dati della pizza ricercata e passarli come model.
+
+// La view a quel punto li mostrerà all’utente con la grafica che preferiamo.
